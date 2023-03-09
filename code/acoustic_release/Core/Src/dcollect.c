e5528cdbd281d9468347801ef04a3aa31e6928a0
kmanikon <103885541+kmanikon@users.noreply.github.com>
2023-03-08 16:54:29 -0800

    Update dcollect.c

--- code/acoustic_release/Core/Src/dcollect.c
+++ code/acoustic_release/Core/Src/dcollect.c
@@ -3,11 +3,14 @@
 
 #define BAUD_RATE 115200
 
+
+// send a single buffer sample to UART (for testing)
 inline void send_sample(uint16_t sample) {
     write_char(sample & 0xFF);
     write_char((sample >> 8) & 0xFF);
 }
 
+// send1 1024 buffer samples over UART
 void send_samples(uint16_t *samples) {
     uint16_t i = 0;
     while (i < 1024) {
@@ -17,9 +20,10 @@ void send_samples(uint16_t *samples) {
     }
 }
 
+// write 8 bytes, all 1's to UART
+// this is the startup confirmation
 void send_ss_seq(void) {
     uint8_t i;
     for (i = 0; i < 8; i++)
         write_char(0xFF);
 }
-
